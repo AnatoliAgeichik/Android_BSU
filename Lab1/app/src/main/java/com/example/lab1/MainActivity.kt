f313381937344f39806af0d61515922be10cd015
AnatoliAgeichik <3anatoliageichik@gmail.com>
2020-10-23 19:43:34 +0300

    fix bugs

--- Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -4,24 +4,18 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
-val EXTRA_KEY:String="123"
+val EXTRA_KEY:String="KEY"
 val REQUEST_CODE:Int=123
-var NUMB=0
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //val textNumb=findViewById<TextView>(R.id.numb)
-        numb.text = NUMB.toString()
+
         btnOk.setOnClickListener {
-            val numb = enterNumb1.text.toString()
+            val numb = numb.text.toString().toInt() + enterNumb1.text.toString().toInt()
             val intent = Intent(this, SecondActivity::class.java).apply {
                 putExtra(EXTRA_KEY, numb)
             }
@@ -33,9 +27,11 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
-                val result = data?.getStringExtra("result")
-                numb.text = result
+                numb.text = data?.getIntExtra(EXTRA_KEY_RETURN,0).toString()
+
             }
         }
     }
 }
+
+
--- Lab1/app/src/main/java/com/example/lab1/SecondActivity.kt
+++ Lab1/app/src/main/java/com/example/lab1/SecondActivity.kt
@@ -4,24 +4,27 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+val EXTRA_KEY_RETURN="returnRes"
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text=intent.getIntExtra(EXTRA_KEY,0)
-        val textView=findViewById<TextView>(R.id.numb)
-        textView.text=text.toString()
+        val text= intent.getIntExtra(EXTRA_KEY,0)
+        numb.text=text.toString()
 
         val enterBtn=findViewById<Button>(R.id.btnOk).setOnClickListener {
             val textNumb=findViewById<EditText>(R.id.enterNumb1)
             val numb=textNumb.text.toString().toInt()
-            NUMB=text+numb
-            val intent= Intent(this, MainActivity::class.java)
+            val intent= Intent().apply {
+                putExtra(EXTRA_KEY_RETURN,text+numb)
+            }
 
             setResult(Activity.RESULT_OK, intent)
             finish()
